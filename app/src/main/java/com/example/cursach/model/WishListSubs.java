afb2af3a8f3f4c0de2995d8865dae11b9dc78573
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-05-05 20:06:07 +0300

    Add wishlist subs class

--- app/src/main/java/com/example/cursach/model/WishListSubs.java
+++ app/src/main/java/com/example/cursach/model/WishListSubs.java
@@ -1,6 +1,14 @@
 package com.example.cursach.model;
 
+import androidx.annotation.NonNull;
+
 public class WishListSubs {
     public int id;
     public int price;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "price " + price;
+    }
 }
