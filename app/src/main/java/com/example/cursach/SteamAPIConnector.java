d70ccd8064e17ecd9034977659195942a0823011
Ilya Korchagin <ilya.korchagin.2014@mail.ru>
2023-05-05 19:57:04 +0300

    Fix game achievements geter

--- app/src/main/java/com/example/cursach/SteamAPIConnector.java
+++ app/src/main/java/com/example/cursach/SteamAPIConnector.java
@@ -8,12 +8,12 @@ import com.lukaspradel.steamapi.core.exception.SteamApiException;
 import com.lukaspradel.steamapi.data.json.achievementpercentages.GetGlobalAchievementPercentagesForApp;
 import com.lukaspradel.steamapi.data.json.ownedgames.Game;
 import com.lukaspradel.steamapi.data.json.ownedgames.GetOwnedGames;
-import com.lukaspradel.steamapi.data.json.playerstats.Achievement;
-import com.lukaspradel.steamapi.data.json.playerstats.GetUserStatsForGame;
+import com.lukaspradel.steamapi.data.json.playerachievements.Achievement;
+import com.lukaspradel.steamapi.data.json.playerachievements.GetPlayerAchievements;
 import com.lukaspradel.steamapi.webapi.client.SteamWebApiClient;
 import com.lukaspradel.steamapi.webapi.request.GetGlobalAchievementPercentagesForAppRequest;
 import com.lukaspradel.steamapi.webapi.request.GetOwnedGamesRequest;
-import com.lukaspradel.steamapi.webapi.request.GetUserStatsForGameRequest;
+import com.lukaspradel.steamapi.webapi.request.GetPlayerAchievementsRequest;
 import com.lukaspradel.steamapi.webapi.request.builders.SteamWebApiRequestFactory;
 
 import java.util.HashMap;
@@ -21,18 +21,11 @@ import java.util.List;
 import java.util.Map;
 
 public class SteamAPIConnector {
-
     private static final String API_KEY = ""; //Steam API KEY
     private static final String TAG = "myTag";
-
     private static SteamWebApiClient client = new SteamWebApiClient.SteamWebApiClientBuilder(API_KEY).build();
 
-/*    public SteamAPIConnector() {
-        client = new SteamWebApiClient.SteamWebApiClientBuilder(API_KEY).build();
-    }*/
-
     public static List<Game> getUserGames(String steamID) throws SteamApiException {
-
         GetOwnedGamesRequest gamesRequest = SteamWebApiRequestFactory.createGetOwnedGamesRequest(steamID);
         GetOwnedGames getOwnedGames = client.<GetOwnedGames>processRequest(gamesRequest);
         List<Game> games = getOwnedGames.getResponse().getGames();
@@ -63,19 +56,25 @@ public class SteamAPIConnector {
         return null;
     }
 
+    /**
+     * @param gameID id игры
+     * @param steamID id пользователя
+     * @return {@link List список} достижений. В additionalProperties хранятся описание и название достижения
+     */
     public static List<Achievement> getUserGameAchievements(int gameID, String steamID) {
-        GetUserStatsForGameRequest userStatsForGameRequest = SteamWebApiRequestFactory
-                .createGetUserStatsForGameRequest(gameID, steamID, "ru");
+        GetPlayerAchievementsRequest achievementsRequest = SteamWebApiRequestFactory
+                .createGetPlayerAchievementsRequest(gameID, steamID, "russian");
         try {
-            GetUserStatsForGame userStatsForGame = client.processRequest(userStatsForGameRequest);
-            return userStatsForGame.getPlayerstats().getAchievements();
+            GetPlayerAchievements playerAchievements = client.processRequest(achievementsRequest);
+            return playerAchievements.getPlayerstats().getAchievements();
         } catch (SteamApiException e) {
+            Log.d(TAG, e.toString());
             //throw new RuntimeException(e);
         }
         return null;
     }
 
-    public static List<com.lukaspradel.steamapi.data.json.achievementpercentages.Achievement> getGameAchievements(int gameID) {
+    public static List<com.lukaspradel.steamapi.data.json.achievementpercentages.Achievement> getGameGlobalAchievements(int gameID) {
         GetGlobalAchievementPercentagesForAppRequest achievementsRequest = SteamWebApiRequestFactory
                 .createGetGlobalAchievementPercentagesForAppRequest(gameID);
         try {
--- app/src/test/java/com/example/cursach/SteamAPIConnectorTest.java
+++ app/src/test/java/com/example/cursach/SteamAPIConnectorTest.java
@@ -2,7 +2,7 @@ package com.example.cursach;
 
 import static org.junit.Assert.*;
 
-import com.lukaspradel.steamapi.data.json.playerstats.Achievement;
+import com.lukaspradel.steamapi.data.json.playerachievements.Achievement;
 import com.lukaspradel.steamapi.webapi.client.SteamWebApiClient;
 
 import org.junit.Before;
@@ -16,13 +16,12 @@ public class SteamAPIConnectorTest {
 
     @Before
     public void setUp() throws Exception {
-        String API_KEY = "6F6A8C94C3E52C64BD799432680ED053"; //Steam API KEY
+        String API_KEY = ""; //Steam API KEY
         SteamWebApiClient client = new SteamWebApiClient.SteamWebApiClientBuilder(API_KEY).build();
     }
 
     @Test
     public void getUserGames() {
-        String steamID = "76561198113659898";
 
     }
 
@@ -37,18 +36,96 @@ public class SteamAPIConnectorTest {
     @Test
     public void getGameAchievements() {
         String steamID = "76561198113659898";
-        int gameID = 632470;
+        int gameID = 286320;
         List<Achievement> expected = new ArrayList<Achievement>() {{
             add(new Achievement() {
                 {
                     setAchieved(1);
-                    setName("ACH_COP_ART");
+                    setApiname("ach_chapter_1");
+                    setAdditionalProperty("unlocktime", 1465919046);
+                    setAdditionalProperty("name", "Awakening");
+                    setAdditionalProperty("description", "Finish chapter 1");
                 }
             });
             add(new Achievement() {
                 {
                     setAchieved(1);
-                    setName("ACH_COP_GOOD");
+                    setApiname("ach_chapter_2");
+                    setAdditionalProperty("unlocktime", 1471447854);
+                    setAdditionalProperty("name", "An extended hand");
+                    setAdditionalProperty("description", "Finish chapter 2");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_chapter_2_boss");
+                    setAdditionalProperty("unlocktime", 1471448721);
+                    setAdditionalProperty("name", "Into the darkness");
+                    setAdditionalProperty("description", "Escape the eyrie");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_chapter_3");
+                    setAdditionalProperty("unlocktime", 1584568831);
+                    setAdditionalProperty("name", "Beneath the surface");
+                    setAdditionalProperty("description", "Finish chapter 3");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_chapter_4");
+                    setAdditionalProperty("unlocktime", 1584572566);
+                    setAdditionalProperty("name", "A winding path");
+                    setAdditionalProperty("description", "Finish chapter 4");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_chapter_5");
+                    setAdditionalProperty("unlocktime", 1584575792);
+                    setAdditionalProperty("name", "Gate of promises");
+                    setAdditionalProperty("description", "Finish chapter 5");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_chapter_5_boss");
+                    setAdditionalProperty("unlocktime", 1584578761);
+                    setAdditionalProperty("name", "Dawn");
+                    setAdditionalProperty("description", "Escape the cavern");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_lore_library");
+                    setAdditionalProperty("unlocktime", 1584578306);
+                    setAdditionalProperty("name", "Folklorist");
+                    setAdditionalProperty("description", "Complete the lore library");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(1);
+                    setApiname("ach_grave");
+                    setAdditionalProperty("unlocktime", 1584572714);
+                    setAdditionalProperty("name", "A final resting place");
+                    setAdditionalProperty("description", "Find the nattramn's grave");
+                }
+            });
+            add(new Achievement() {
+                {
+                    setAchieved(0);
+                    setApiname("ach_mystery_carving");
+                    setAdditionalProperty("unlocktime", 0);
+                    setAdditionalProperty("name", "Mystery carving");
+                    setAdditionalProperty("description", "It's a secret to everybody");
                 }
             });
         }};
